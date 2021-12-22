4e79ff7da51dde2520ba921cadafcc4db879f565
hieuadoan <hieu.a.doan@gmail.com>
2021-12-22 15:34:19 -0600

    add GPR and AFs

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -1,1 +1,55 @@
 import numpy as np
+from scipy.stats import norm
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import ConstantKernel as C ,WhiteKernel as Wht,Matern as matk
+
+def GPRegression(Xtrain, Ytrain, Nfeature):    
+    cmean=[1.0]*Nfeature
+    cbound=[[1e-3, 1e3]]*Nfeature
+    kernel = C(1.0, (1e-3,1e3)) * matk(cmean,cbound,1.5) + Wht(1.0, (1e-3, 1e3))  # Matern kernel
+    gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=40, normalize_y=False)
+    gp.fit(Xtrain, Ytrain)
+    return gp
+
+def GPPrediction(gpnetwork, xtest):
+    y_pred, sigma = gpnetwork.predict(xtest, return_std=True)
+    return y_pred, sigma
+
+def upperConfidenceBound(xdata, gpnetwork, epsilon):
+    """
+        xdata: input feature vectors or PCs of the REMAINING set
+        gpnetwork: GPR model
+        epsilon: control exploration/exploitation. Higher epsilon means more exploration
+    """
+    ye_pred, esigma = gprediction(gpnetwork, xdata)
+    ucb = np.empty(ye_pred.size, dtype=float)
+    for ii in range(0,ye_pred.size):
+        if esigma[ii] > 0:
+            ucb[ii]=(ye_pred[ii]+epsilon*esigma[ii])
+        else:
+            ucb[ii]=0.0
+    return ucb
+
+def probabilityOfImprovement(xdata, gpnetwork, ybest, epsilon):  
+    "ybest: GPR-predicted best output property of the TRAINING set"
+
+    ye_pred, esigma = gprediction(gpnetwork, xdata)
+    poI = np.empty(ye_pred.size, dtype=float)
+    for ii in range(0,ye_pred.size):
+        if esigma[ii] > 0:
+            zzval=(ye_pred[ii]-ybest-epsilon)/float(esigma[ii])
+            poI[ii]=norm.cdf(zzval)
+        else:
+            poI[ii]=0.0
+    return poI
+
+def expectedImprovement(xdata,gpnetwork,ybest,epsilon): 
+    ye_pred, esigma = gprediction(gpnetwork, xdata)
+    expI = np.empty(ye_pred.size, dtype=float)
+    for ii in range(0,ye_pred.size):
+        if esigma[ii] > 0:
+            zzval=(ye_pred[ii]-ybest)/float(esigma[ii])
+            expI[ii]=(ye_pred[ii]-ybest-epsilon)*norm.cdf(zzval)+esigma[ii]*norm.pdf(zzval)
+        else:
+            expI[ii]=0.0
+    return expI
