6bc024a15cb389af6883953f9774204d9063c293
hieuadoan <hieu.a.doan@gmail.com>
2023-04-13 09:11:31 -0500

    Fixed Smiles2Fingerprint function to work with all fingerprints

--- src/utils/chem_utils.py
+++ src/utils/chem_utils.py
@@ -17,7 +17,7 @@ class FingerprintGenerator:
     ''' Generate the fingerprint for a molecule, given the fingerprint type
     Parameters: 
         mol (rdkit.Chem.rdchem.Mol) : RdKit mol object (None if invalid smile string smi)
-        fp_type (string)            : Fingerprint type  (choices: AtomPair|Pharmacophore|ECFP4|ECFP6|FCFP4|FCFP6)  
+        fp_type (string)            : Fingerprint type  (choices: AtomPair|Pharmacophore|RDK|ECFP4|ECFP6|FCFP4|FCFP6|PhysChemFeatures)  
     Returns:
         Bit vector (of size 2048 by default)
     '''
@@ -28,35 +28,69 @@ class FingerprintGenerator:
         if method is None:
             raise Exception(f'{fp_type} is not a supported fingerprint type.')
         return method(mol)
-
-    def get_AtomPair(self, mol: Chem.rdchem.Mol):
-        return np.array(Pairs.GetAtomPairFingerprintAsBitVect(mol))
+    
+    def get_AtomPairs(self, mol: Chem.rdchem.Mol, numBits=2048):
+        atom_pair_hashed = Pairs.GetHashedAtomPairFingerprint(mol, nBits=numBits)
+        numpy_array = np.zeros(numBits, dtype=np.int32)
+        for index, value in atom_pair_hashed.GetNonzeroElements().items():
+            numpy_array[index] = value
+        return np.array(['AtomPairs']), numpy_array
 
     def get_Pharmacophore(self, mol: Chem.rdchem.Mol):
-        return np.array(Generate.Gen2DFingerprint(mol, Gobbi_Pharm2D.factory))
-
-    def get_BPF(self, mol: Chem.rdchem.Mol):
-        return np.araay(GetBPFingerprint(mol))
-
-    def get_BTF(self, mol: Chem.rdchem.Mol):
-        return np.array(GetBTFingerprint(mol))
+        return np.array(['Pharmacophore']), np.array(Generate.Gen2DFingerprint(mol, Gobbi_Pharm2D.factory))
 
     def get_RDK(self, mol: Chem.rdchem.Mol):
-        return np.array(AllChem.RDKFingerprint(mol))
+        return np.array(['RDK']), np.array(AllChem.RDKFingerprint(mol))
 
-    def get_ECFP4(self, mol: Chem.rdchem.Mol):
-        return np.array(AllChem.GetMorganFingerprintAsBitVect(mol, 2))
+    def get_ECFP4(self, mol: Chem.rdchem.Mol): 
+        """ Morgan fingperint at radius equal to 2 """
+        return np.array(['ECFP4']), np.array(AllChem.GetMorganFingerprintAsBitVect(mol, 2))
 
     def get_ECFP6(self, mol: Chem.rdchem.Mol):
-        return np.array(AllChem.GetMorganFingerprintAsBitVect(mol, 3))
+        """ Morgan fingerprint at radius equal to 3 """
+        return np.array(['ECFP6']), np.array(AllChem.GetMorganFingerprintAsBitVect(mol, 3))
 
     def get_FCFP4(self, mol: Chem.rdchem.Mol):
-        return np.array(AllChem.GetMorganFingerprintAsBitVect(mol, 2, useFeatures=True))
+        return np.array(['FCFP4']), np.array(AllChem.GetMorganFingerprintAsBitVect(mol, 2, useFeatures=True))
 
     def get_FCFP6(self, mol: Chem.rdchem.Mol):
-        return np.array(AllChem.GetMorganFingerprintAsBitVect(mol, 3, useFeatures=True))
+        return np.array(['FCFP6']), np.array(AllChem.GetMorganFingerprintAsBitVect(mol, 3, useFeatures=True))
+	
+    def get_PhysChemFeats(self, mol: Chem.rdchem.Mol):
+        """
+        Features used in Doan et al. Chem. Mat. 2020
+        """
+        FastFindRings(mol)
+        descNameList = []
+        descValList = []
+        funcList = [MolDescriptors, Descriptors, Lipinski, Crippen, Fragments]
+        for func in funcList:
+            if func == Fragments:
+                attrs = [getattr(func, name) for name in dir(func) if "fr" in name]
+                funcNames = [name for name in dir(func) if "fr" in name]
+            if func == Lipinski:
+                attrs = [getattr(func, name) for name in dir(func) if ("Count" in name and "Smarts" not in name)]
+                funcNames = [name for name in dir(func) if ("Count" in name and "Smarts" not in name)]
+            if func == MolDescriptors:
+                attrs = [getattr(func, name) for name in dir(func)
+                if (("CalcNum" in name or "CalcExact" in name or "CalcTPSA" in name or "CalcChi" in name or "Kappa" in name or "Labute" in name)
+                and "Stereo" not in name and "_" not in name and "ChiN" not in name)]
+                funcNames = [name for name in dir(func)
+                if (("CalcNum" in name or "CalcExact" in name or "CalcTPSA" in name or "CalcChi" in name or "Kappa" in name or "Labute" in name)
+                and "Stereo" not in name and "_" not in name and "ChiN" not in name)]
+            if func == Descriptors:
+                attrs = [getattr(func, name) for name in dir(func) if ("Num" in name or "Min" in name or "Max" in name)]
+                funcNames = [name for name in dir(func) if ("Num" in name or "Min" in name or "Max" in name)]
+            if func == Crippen:
+                attrs = [getattr(func, name) for name in dir(func) if (("MolLogP" in name or "MolMR" in name) and "_" not in name)]
+                funcNames = [name for name in dir(func) if (("MolLogP" in name or "MolMR" in name) and "_" not in name)]
+            for name, attr in zip(funcNames,attrs):
+                descValList.append(attr(mol))
+                descNameList.append(name)
+        return np.array(descNameList), np.array(descValList)
 
-def get_fingerprint(mol: Chem.rdchem.Mol, fp_type: str):
+def get_fingerprint(smiles: str, fp_type: str):
+
     ''' Fingerprint getter method. Fingerprint is returned after using object of 
         class 'FingerprintGenerator'
         
@@ -67,62 +101,28 @@ def get_fingerprint(mol: Chem.rdchem.Mol, fp_type: str):
         RDKit fingerprint object
         
     '''
-    return FingerprintGenerator().get_fingerprint(mol=mol, fp_type=fp_type)
+    return FingerprintGenerator().get_fingerprint(mol=Chem.MolFromSmiles(smiles), fp_type=fp_type)
 
-def get_RDKitFeatures(smiles: str) -> np.ndarray:
-    """
-    Input: SMILES
-
-    Output: An 1D array of molecular descriptors from
-    'Quantum Chemistry-Informed Active Learning to Accelerate the Design and Discovery of Sustainable Energy Storage Materials'
-    Doan et al.
-    """
-    mol = Chem.MolFromSmarts(smiles,False)
-    mol.UpdatePropertyCache()
-    FastFindRings(mol)
-    descNameList = []
-    descValList = []
-    funcList = [MolDescriptors, Descriptors, Lipinski, Crippen, Fragments]
-    for func in funcList:
-        if func == Fragments:
-            attrs = [getattr(func, name) for name in dir(func) if "fr" in name]
-            funcNames = [name for name in dir(func) if "fr" in name]
-        if func == Lipinski:
-            attrs = [getattr(func, name) for name in dir(func) if ("Count" in name and "Smarts" not in name)]
-            funcNames = [name for name in dir(func) if ("Count" in name and "Smarts" not in name)]
-        if func == MolDescriptors:
-            attrs = [getattr(func, name) for name in dir(func) 
-            if (("CalcNum" in name or "CalcExact" in name or "CalcTPSA" in name or "CalcChi" in name or "Kappa" in name or "Labute" in name) 
-            and "Stereo" not in name and "_" not in name and "ChiN" not in name)]
-            funcNames = [name for name in dir(func) 
-            if (("CalcNum" in name or "CalcExact" in name or "CalcTPSA" in name or "CalcChi" in name or "Kappa" in name or "Labute" in name) 
-            and "Stereo" not in name and "_" not in name and "ChiN" not in name)]
-        if func == Descriptors:
-            attrs = [getattr(func, name) for name in dir(func) if ("Num" in name or "Min" in name or "Max" in name)]
-            funcNames = [name for name in dir(func) if ("Num" in name or "Min" in name or "Max" in name)]
-        if func == Crippen:
-            attrs = [getattr(func, name) for name in dir(func) if (("MolLogP" in name or "MolMR" in name) and "_" not in name)]
-            funcNames = [name for name in dir(func) if (("MolLogP" in name or "MolMR" in name) and "_" not in name)]
-        for name, attr in zip(funcNames,attrs):
-            descValList.append(attr(mol))
-            descNameList.append(name)
-    return np.array(descNameList), np.array(descValList)
-
-def Smiles2Fingerprint(smilesList) -> pd.DataFrame:
+def Smiles2Fingerprint(smilesList, fp_type='PhysChemFeats') -> pd.DataFrame:
     """
     Input: List of SMILES
     Output: Dataframe of molecular descriptors
     """
     
-    SmilesDescriptors = [get_RDKitFeatures(sml) for sml in smilesList]
+    SmilesDescriptors = [get_fingerprint(sml,fp_type) for sml in smilesList]
     descValues = [list(l[1]) for l in SmilesDescriptors]
-    descNames = SmilesDescriptors[0][0]
+    if fp_type=='PhysChemFeats':
+        descNames = SmilesDescriptors[0][0]
+    else:
+        descNames = np.arange(len(descValues[0]))
+        print(descNames)
     dfFeatures = pd.DataFrame(descValues, columns=descNames)
-
     return dfFeatures
 
 if __name__ == '__main__':
-    sml = 'CCOCCO'
-    fp = get_fingerprint(Chem.MolFromSmiles(sml),'ECFP4')
+    #sml = 'O'
+    smlList = ['O','COC']
+    #_, fp = get_fingerprint(sml, 'AtomPairs')
+    fp = Smiles2Fingerprint(smlList,'ECFP6')
     print(fp)
     print(len(fp))
